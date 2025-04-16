72fc904aefc1374cbd8371f5276c7e43c6695392
Jens Klimke <jens.klimke@rwth-aachen.de>
2025-04-16 15:04:56 +0200

    version set to 0.1.0

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='datify',
-    version='0.1.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'markdown',
-        'pyyaml',
-        'Jinja2'
-    ],
+    name="datify",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["markdown", "pyyaml", "Jinja2"],
     entry_points={
-        'console_scripts': [
-            'datify=datify:main',
+        "console_scripts": [
+            "datify=datify:main",
         ],
     },
 )
